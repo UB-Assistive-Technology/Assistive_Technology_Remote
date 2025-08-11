077b00d40eeadee53616c3ccd5398e160eca8a56
Orniepay <payerornie@gmail.com>
2025-08-11 19:32:46 -0400

    Javascript added event listeners for power, volume, and channel buttons with inline named handlers

---
+++ index.js
@@ -0,0 +1,28 @@
+// Power button 
+document.querySelector('.power-btn').addEventListener('click', function powerButtonHandler(){
+    console.log("Power button pressed");
+    // TODO: connect to Python script
+});
+
+// Volume buttons
+document.querySelector('.volume-plus-btn').addEventListener('click', function volumePlusHandler(){
+    console.log("Volume + button pressed");
+    // TODO: connect to Python script
+});
+
+document.querySelector('.volume-minus-btn').addEventListener('click', function volumeMinusHandler(){
+    console.log("Volume - button pressed");
+    // TODO: connect to Python script
+});
+
+
+// Channel buttons 
+document.querySelector('.channel-up-btn').addEventListener('click', function channelUpHandler(){
+    console.log("Channel up pressed");
+    // TODO: connect to Python script
+});
+
+document.querySelector('.channel-down-btn').addEventListener('click', function channelDownHandler(){
+    console.log("Channel down button pressed");
+    // TODO: connect to Python script
+});
