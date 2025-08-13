902b45531594404fe2591abb40d4c0fbdd096f7a
Orniepay <payerornie@gmail.com>
2025-08-13 14:02:43 -0400

    Uncommented serial communication to arduino to test on Raspberry Pi end

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ import eel
 import serial
 import webview
 
-# # Start serial communication to arduino
-# # *** Make sure your port and baud rate is correct ***
-# ser = serial.Serial('COM9', 115200) # This line is for PC testing purposes
-# # ser = serial.Serial("/dev/ttyACM0", 115200) # This line is for Raspberry Pi 
+# Start serial communication to arduino
+# *** Make sure your port and baud rate is correct ***
+ser = serial.Serial('COM9', 115200) # This line is for PC testing purposes
+# ser = serial.Serial("/dev/ttyACM0", 115200) # This line is for Raspberry Pi 
 
-# # Initialize Eel Library
-# eel.init('web')
+# Initialize Eel Library
+eel.init('web')
 
-# # Eel API allows data to be sent on serial communication
-# @eel.expose
-# def send_data(message):
-#     ser.write((message + '\n').encode())
-#     print(f"Sent to Arduino: {message}")
+# Eel API allows data to be sent on serial communication
+@eel.expose
+def send_data(message):
+    ser.write((message + '\n').encode())
+    print(f"Sent to Arduino: {message}")
 
 webview.create_window(
     "Assistive Technology Remote",
