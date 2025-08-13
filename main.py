06a8a16bb5aedabe35600480533735a55f70a08b
Orniepay <payerornie@gmail.com>
2025-08-13 14:51:49 -0400

    Reverted from pywebview to eel library use case for respective standalone app window application. Added ser = None placeholder for Windows testing to bypass serial init and prevent errors when Arduino is not connected.

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import serial
 
 # Start serial communication to arduino
 # *** Make sure your port and baud rate is correct ***
-ser = serial.Serial('COM9', 115200) # This line is for PC testing purposes
+# ser = serial.Serial('COM9', 115200) # This line is for PC testing purposes
 # ser = serial.Serial("/dev/ttyACM0", 115200) # This line is for Raspberry Pi 
+
+# Placeholder value for testing on Windows without an Arduino; prevents SerialException and allows UI to load
+ser = None 
 
 # Initialize Eel Library
 eel.init('web')
