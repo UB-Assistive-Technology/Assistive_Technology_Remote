1c67400f29cb28cac68b7571130939fb600dd3c9
Orniepay <payerornie@gmail.com>
2025-08-13 13:59:35 -0400

    Integrated PyWebView for standalone app window and updated asset paths to relative for local file loading

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import eel
 import serial
+import webview
 
-# Start serial communication to arduino
-# *** Make sure your port and baud rate is correct ***
-ser = serial.Serial('COM9', 115200) # This line is for PC testing purposes
-# ser = serial.Serial("/dev/ttyACM0", 115200) # This line is for Raspberry Pi 
+# # Start serial communication to arduino
+# # *** Make sure your port and baud rate is correct ***
+# ser = serial.Serial('COM9', 115200) # This line is for PC testing purposes
+# # ser = serial.Serial("/dev/ttyACM0", 115200) # This line is for Raspberry Pi 
 
-# Initialize Eel Library
-eel.init('web')
+# # Initialize Eel Library
+# eel.init('web')
 
-# Eel API allows data to be sent on serial communication
-@eel.expose
-def send_data(message):
-    ser.write((message + '\n').encode())
-    print(f"Sent to Arduino: {message}")
+# # Eel API allows data to be sent on serial communication
+# @eel.expose
+# def send_data(message):
+#     ser.write((message + '\n').encode())
+#     print(f"Sent to Arduino: {message}")
 
-# Start hosting index.html with Eel
-eel.start('index.html', size=(400, 800))
+webview.create_window(
+    "Assistive Technology Remote",
+    "web/index.html",
+    width=480,
+    height=800,
+    resizable=False
+)
+
+webview.start()
